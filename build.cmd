4b508af3ac700ea81f00c52c1fe5f8e616130d7f
BuildTools <unconfigured@null.spigotmc.org>
2016-05-28 11:43:10 -0400

    Fixes

--- build.cmd
+++ build.cmd
@@ -6,5 +6,10 @@ echo github.com/tlc-corp/PocketForge
 echo $channel
 echo.
 echo.
+echo Bundling package...
+echo [DONE!]
 echo Starting server...
 bin\php\php.exe -c bin\php src\pocketmine\PocketMine.php %*
+echo system> [INFO]: System has shut down.
+pause
+exit
